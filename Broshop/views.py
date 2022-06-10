387ec6dae2af81362035c454ffc2c6ef7beb3a34
lybu-4612 <lybu4612@gmail.com>
2022-06-10 21:35:40 +0530

    for reviews

--- Broshop/views.py
+++ Broshop/views.py
@@ -8,16 +8,22 @@ from store.models import Product,ReviewRating
 # Create your views here.
 
 def home(request):
+        try:
+                products = Product.objects.all().filter(is_available=True).order_by('-created_date')
         
-        products = Product.objects.all().filter(is_available=True).order_by('-created_date')
-        for product in products:
-                reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
-        context = {
-                'products': products,
-                'reviews': reviews,
-        }
+                for product in products:
+                        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
+                context = {
+                        'products': products,
+                        'reviews': reviews,
+                }
+                return render(request, 'home.html', context)
+
+        except:
+                return render(request, 'home.html')
+                
         
-        return render(request, 'home.html', context)
+
 
         
 
